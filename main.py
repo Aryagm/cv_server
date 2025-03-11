fe04d632d3f789d9ecad3f422bd966a008ac93f6
Arya Manjaramkar <aryagm01@gmail.com>
2025-03-11 12:38:09 -0400

    Refactor boundary alert logic and comment out off-sidewalk alerts

--- main.py
+++ main.py
@@ -392,17 +392,17 @@ async def process_frame(frame_data: FrameData):
                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
         
         # Add directional guidance based on position
-        if off_sidewalk:
-            # Critical alert for off sidewalk
-            add_alert(alerts, "WARNING: You are off the sidewalk!")
+        # if off_sidewalk:
+        #     # Critical alert for off sidewalk
+        #     add_alert(alerts, "WARNING: You are off the sidewalk!")
             
-            if user_point[0] < left_boundary:
-                cv2.putText(processed_img, "MOVE RIGHT", (width//2 - 80, height - 80),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
-            else:
-                cv2.putText(processed_img, "MOVE LEFT", (width//2 - 80, height - 80),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
-        elif near_left_boundary:
+        #     if user_point[0] < left_boundary:
+        #         cv2.putText(processed_img, "MOVE RIGHT", (width//2 - 80, height - 80),
+        #                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+        #     else:
+        #         cv2.putText(processed_img, "MOVE LEFT", (width//2 - 80, height - 80),
+        #                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+        if near_left_boundary:
             # Warning alert for approaching left boundary
             add_alert(alerts, "Caution: Getting close to left edge")
             cv2.putText(processed_img, "DRIFT RIGHT", (width//2 - 80, height - 80),
